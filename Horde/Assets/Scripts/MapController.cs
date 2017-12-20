3033c9ff9ba3ec63ada402154099fb88d00dbbdc
Hild Franck <franck.hild@gmail.com>
2017-12-20 03:11:13 +0100

    Create grid

---
+++ Horde/Assets/Scripts/MapController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour {
+	public float cellSize = 32;
+	public float xCell = 10f;
+	public float yCell = 10f;
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector3(xCell, yCell, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
