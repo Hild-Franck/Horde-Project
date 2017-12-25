936a1a90704daeed84bc29f7f21c1a6544695739
Knarfux <franck.hild@gmail.com>
2017-12-25 19:23:27 +0100

    Camera movements

---
+++ Horde/Assets/Scripts/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float speed = 5f;
+
+	private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rigidbody.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
+	}
+}
