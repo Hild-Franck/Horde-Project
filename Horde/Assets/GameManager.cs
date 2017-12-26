29035f5212647d7c57a411bbf471ed5f1087c239
Hild Franck <franck.hild@gmail.com>
2017-12-27 00:01:28 +0100

    Setup Player

---
+++ Horde/Assets/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	public GameObject player;
+	public GameObject RTS;
+
+	public Camera RTSCamera;
+	public Camera FPSCamera;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+	}
+
+	void Start () {
+		player.SetActive(false);
+		RTSCamera.enabled = true;
+		FPSCamera.enabled = false;
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown("SwitchMode")) {
+			player.SetActive(true);
+			RTS.SetActive(false);
+			
+			RTSCamera.enabled = false;
+			FPSCamera.enabled = true;
+		}
+	}
+}
