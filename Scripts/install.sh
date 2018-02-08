e64a619d2fed429b864bfd0a7026836f97f602f2
Knarfux <franck.hild@gmail.com>
2018-02-08 16:40:19 +0100

    Fix output pkg file

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -5,7 +5,7 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading from https://download.unity3d.com/download_unity/a9f86dcd79df/MacEditorInstaller/Unity-2017.3.0f3.pkg: '
-curl -o https://download.unity3d.com/download_unity/a9f86dcd79df/MacEditorInstaller/Unity-2017.3.0f3.pkg
+curl -o Unity.pkg https://download.unity3d.com/download_unity/a9f86dcd79df/MacEditorInstaller/Unity-2017.3.0f3.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
