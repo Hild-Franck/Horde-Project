075ed5ae7a1a598d0ba7b950db0ca3927a11c907
Knarfux <franck.hild@gmail.com>
2018-02-09 12:49:39 +0100

    Update Unity build version to latest

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,14 +4,14 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-echo 'Downloading from http://netstorage.unity3d.com/unity/e87ab445ead0/MacEditorInstaller/Unity.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/e87ab445ead0/MacEditorInstaller/Unity.pkg
+echo 'Downloading from https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg: '
+curl -o Unity.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
 
-echo 'Downloading from http://netstorage.unity3d.com/unity/e87ab445ead0/MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-5.3.2f1.pkg: '
-curl -o MacSupport.pkg http://netstorage.unity3d.com/unity/e87ab445ead0/MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-5.3.2f1.pkg
+echo 'Downloading from https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.3.1f1.pkg: '
+curl -o MacSupport.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.3.1f1.pkg
 
 echo 'Installing MacSupport.pkg'
 sudo installer -dumplog -package MacSupport.pkg -target /
