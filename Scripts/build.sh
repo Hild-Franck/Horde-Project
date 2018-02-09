9891d01ba315cae22be8e06834bc4b5ee93e2a5d
Knarfux <franck.hild@gmail.com>
2018-02-09 10:08:00 +0100

    Improve deployment

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,7 +4,7 @@
 
 # Change this the name of your project. This will be the name of the final executables as well.
 project="horde-project"
-build="$project-$TRAVIS_BRANCH-$TRAVIS_COMMIT"
+build="$project-$TRAVIS_BRANCH-$TRAVIS_TAG"
 
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -13,7 +13,7 @@ echo "Attempting to build $project for Windows"
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$build.exe" \
+  -buildWindowsPlayer "$(pwd)/Deployment/Builds/windows/$build.exe" \
   -quit
 
 echo "Attempting to build $project for OS X"
@@ -23,7 +23,7 @@ echo "Attempting to build $project for OS X"
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$build.app" \
+  -buildOSXUniversalPlayer "$(pwd)/Deployment/Builds/osx/$build.app" \
   -quit
 
 echo "Attempting to build $project for Linux"
@@ -33,7 +33,7 @@ echo "Attempting to build $project for Linux"
   -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$build.exe" \
+  -buildLinuxUniversalPlayer "$(pwd)/Deployment/Builds/linux/$build" \
   -quit
 
 echo 'Logs from build'
