385df76cc0455bb499942b54fc9c49c9ac4d5e34
Knarfux <franck.hild@gmail.com>
2019-05-18 19:56:09 +0200

    Add wall animation on build

---
+++ Assets/WallPart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPart : MonoBehaviour {
+		public void StartAnimation(float waitingAnimation) {
+			StartCoroutine(WaitAndAnimate(waitingAnimation));
+		}
+		private IEnumerator WaitAndAnimate(float waitingAnimation) {
+			yield return new WaitForSeconds(waitingAnimation);
+			GetComponent<Animation>().Play();
+	}
+}
--- Assets/WallPartGhost.cs
+++ Assets/WallPartGhost.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class WallPartGhost : MonoBehaviour {
 	public GameObject wallPartPrefab;
 
-    public void Build(GameObject parent) {
+    public GameObject Build(GameObject parent) {
 			GameObject instance = Instantiate(wallPartPrefab, Vector3.zero, Quaternion.identity);
 			instance.transform.parent = parent.transform;
 			instance.transform.localPosition = transform.localPosition;
 			instance.transform.localRotation = transform.localRotation;
+			return instance;
 	}
 }
