e28e7a041764af0d31f18ec526a583aaebc7026b
Knarfux <franck.hild@gmail.com>
2019-05-21 20:23:13 +0200

    Add some methods and properties

--- Assets/WallPart.cs
+++ Assets/WallPart.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WallPart : MonoBehaviour {
-		public void StartAnimation(float waitingAnimation) {
-			StartCoroutine(WaitAndAnimate(waitingAnimation));
-		}
-		private IEnumerator WaitAndAnimate(float waitingAnimation) {
-			yield return new WaitForSeconds(waitingAnimation);
-			GetComponent<Animation>().Play();
+	public float health;
+	public int upgradeCost;
+	public int upgradeLevel;
+
+	public void StartAnimation(float waitingAnimation) {
+		StartCoroutine(WaitAndAnimate(waitingAnimation));
+	}
+
+	public void TakeDamage(float damage) {
+		health -= damage;
+	}
+
+	public void DestroyBuilding() {
+
+	}
+
+	public void Upgrade() {
+		
+	}
+
+	private IEnumerator WaitAndAnimate(float waitingAnimation) {
+		yield return new WaitForSeconds(waitingAnimation);
+		GetComponent<Animation>().Play();
 	}
 }
