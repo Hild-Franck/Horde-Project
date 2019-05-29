e297d2ad011ac6cd384bf0632738aa9084295f37
Knarfux <franck.hild@gmail.com>
2019-05-29 22:42:46 +0200

    Basically AiO commit. TG-5 #en-pause

---
+++ Assets/DamagedSpike.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagedSpike : MonoBehaviour {
+  public GameObject spikeTop;
+  void Start() {
+    spikeTop.GetComponent<Rigidbody>().AddTorque((Vector3.right * 75) + Vector3.forward * Random.Range(-50f, 50f));
+  }
+}
