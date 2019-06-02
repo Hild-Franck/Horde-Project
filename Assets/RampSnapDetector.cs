4d70839f3266f33958679cc7af7a505e491669e1
Knarfux <franck.hild@gmail.com>
2019-06-02 12:18:23 +0200

    Add snap detector to ramp. TG-9 #in-progress

---
+++ Assets/RampSnapDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RampSnapDetector : MonoBehaviour
+{
+  private bool detected = false;
+
+  void Update() {
+    bool wasDetected = detected;
+    detected = true;
+    foreach (Transform child in transform)
+    {
+      if (!child.GetComponent<SnapDetectorTrigger>().triggered) detected = false;
+    }
+    if (detected) {
+      foreach (Transform child in transform) {
+        child.GetComponent<SnapDetectorTrigger>().HideObject();
+      }
+    }
+  }
+
+  public bool CheckSnapping() => detected;
+
+  public void RemoveObjects() {
+    foreach (Transform child in transform) {
+      child.GetComponent<SnapDetectorTrigger>().RemoveObject();
+    }
+  }
+}
